54e87afe37129f1b29a5fb2f7d9034fa86100296
brunoguez <brunomguez@gmail.com>
2024-08-27 21:32:46 -0300

    2708

--- ConexaoBanco/src/Conexao.java
+++ ConexaoBanco/src/Conexao.java
@@ -2,8 +2,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Conexao {
-    private static final String url = "jdbc:mysql://localhost:3306/bd01";
-    private static final String user = "root";
+    private static final String url = "jdbc:mysql://52.255.187.179:3306/bd01";
+    private static final String user = "brunoguez";
     private static final String pwd = "root";
     private static Connection conn;
 
--- ConexaoBanco/src/UsuarioDAO.java
+++ ConexaoBanco/src/UsuarioDAO.java
@@ -3,7 +3,7 @@ import java.sql.PreparedStatement;
 public class UsuarioDAO {
     public void cadastrarUsuario(Usuario Usuario) {
         //String sql = "INSERT INTO USUARIO(NOME, USUARIO, SENHA, EMAIL) VALUES("+"Eduardo"+","+"etda"+","+"123"+","+"etda@etda.com"+")";
-        String sql = "INSERT INTO USUARIO(NOME, USUARIO, SENHA, EMAIL) VALUES(?,?,?,?)";
+        String sql = "INSERT INTO usuario(NOME, USUARIO, SENHA, EMAIL) VALUES(?,?,?,?)";
         PreparedStatement ps = null;
         try {
             ps = Conexao.getConexao().prepareStatement(sql);
